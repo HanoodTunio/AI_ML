a697ff0988bf6a5b8da1a9c35b1ad8d1087023be
HanoodTunio <hanood.tunio@gmail.com>
2024-02-29 22:58:31 +0500

    Added ClassObject  Practice

---
+++ basics_of_python/ClassObject.py
@@ -0,0 +1,21 @@
+print("Example 01")
+class MyClass:
+    x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+print("Example 02")
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+print(p1.name)
+print(p1.age)
+p1.myfunc()
+
